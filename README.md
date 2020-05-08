648ee282811bde9d126dde58c677c08adc11a7b6
younghoon <younghoon@naver.com>
2020-05-08 15:40:03 +0900

    Init
    init 작업중입니다.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Guess MINE
+
+리얼타임 드로잉 게임!! SocketIO와 GULP & NODE로 작성하였습니다.
+
+## To Do : 
+- [X] Server
+- [X] Pug
+- [X] Static
+- [ ] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 import express from "express";
-
-
-// const express = require('express');    
-
-
+import {join} from 'path';
+import path from 'path';
+import socketIO from "socket.io";
 const PORT = 4000 || null;
 const app = express();
+app.set('view engine','pug');
+// app.set('views',join(__dirname,"views"));
+console.log(__dirname);
 
-const handleListening = () => console.log("서버 재부팅~");
+//아래 3개는 동일한 소스코드이다. 
+// app.set('views',"C:\\Users\\wachsenhaus\\Documents\\guess-MINE\\src\\views")
+app.set('views',`${__dirname}\\views`);
+// app.set('views',path.join(__dirname,"views"));
+
+
+app.use(express.static(join(__dirname,"static")));
+// app.get("/", (req,res) => res.render("home"));
+app.get("/", openHome);
+
+function openHome(req,res)
+{
+    res.render("home");
+}
+
+const handleListening = () => console.log(`✔ Server running : http://localhost:${PORT}`);
 
 app.listen(PORT,handleListening);
