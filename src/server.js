eca55d6e7ed7b02f002084c8c1d852e0893c0ed8
younghoon <younghoon@naver.com>
2020-05-08 18:21:18 +0900

    Test code Added
    테스트코드를 추가했습니다.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 import express from "express";
-import {join} from 'path';
 import path from 'path';
+import {join} from 'path';
+
 import socketIO from "socket.io";
-const PORT = 4000 || null;
+const PORT = 4001 || null;
 const app = express();
 app.set('view engine','pug');
-// app.set('views',join(__dirname,"views"));
-console.log(__dirname);
 
+console.log(__dirname);
+// console.log(path.normalize("C:\\\\\\\\Users\\..\\.\\guess-MINE\\src\\views"))
+// console.log(path.resolve("aas\\..\\.\\wawawa\\guess-MINE\\src\\views"))
+console.log(__filename);
+console.log(path.basename(__filename,path.extname(__filename)));
 //아래 3개는 동일한 소스코드이다. 
+// app.set('views',join(__dirname,"views"));
 // app.set('views',"C:\\Users\\wachsenhaus\\Documents\\guess-MINE\\src\\views")
 app.set('views',`${__dirname}\\views`);
+// app.set('views',`${path.normalize("C:\\\\\\\\Users\\..\\.\\guess-MINE\\src")}\\views`);
 // app.set('views',path.join(__dirname,"views"));
 
 
